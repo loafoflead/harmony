011d03186200371ac09eca5253f12cdd26f4d6fb
asyncvoid <benjyreed59@gmail.com>
2021-12-17 20:52:54 +0000

    first commit: working server

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace networkingTestCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	ServerTest3 server = new ServerTest3();
+        }
+    }
+}
